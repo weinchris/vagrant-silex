4e5f4d863f2ac5d4d160b89a46798723685931c6
Chris <christof@weinreich-edv.de>
2016-03-17 00:57:04 +0100

    smoothed out the dinglebop

--- silex/sql/datadefinition.sql
+++ silex/sql/datadefinition.sql
@@ -1,3 +1,16 @@
 USE silex;
 
 -- CREATE YOUR TABLES HERE
+CREATE TABLE blog_post (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  author VARCHAR(255),
+  title VARCHAR(255),
+  text TEXT,
+  created_at DATE
+);
+
+CREATE TABLE users (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  username VARCHAR(255),
+  password VARCHAR(255)
+);
--- silex/sql/datamanipulation.sql
+++ silex/sql/datamanipulation.sql
@@ -1,3 +1,5 @@
 USE silex;
 
 -- PREFILL YOUR TABLES HERE
+INSERT INTO blog_post (title, text, author, created_at) VALUES ('New Title', 'This is the content', 'master', CURRENT_DATE);
+INSERT INTO users (username, password) VALUES ('master', 'abc');
